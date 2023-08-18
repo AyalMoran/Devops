31b894bc7f4ca975ba3f895c0dd80ae18569a59b
AyalMoran <moranayal@gmail.com>
2023-08-18 22:01:05 +0300

    moranayal

--- Ayal.java
+++ Ayal.java
@@ -1,7 +1,7 @@
 public class Ayal {
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println("Ayal Moran ..."+i);
+            System.out.println("MoranAyal ..."+i);
         }
     }
 }
